1d87db4cd8364f38f3a1e8c9649db000cea07b38
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-23 21:38:07 +0530

    complete theme change

--- content.js
+++ content.js
@@ -4,7 +4,6 @@ function toggleDarkTheme() {
 	document.documentElement.style.setProperty("--surface-secondary", "black");
 	document.documentElement.style.setProperty("--surface-primary", "black");
 	document.documentElement.style.setProperty("--surface-tertiary", "black");
-	// document.documentElement.style.setProperty("font-size", "18px");
 }
 
 toggleDarkTheme();
@@ -12,8 +11,5 @@ toggleDarkTheme();
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.action === "toggleDarkTheme") {
 		toggleDarkTheme();
-	} else if (request.action === "changeFontSize") {
-		const fontSize = request.fontSize + "px";
-		document.documentElement.style.setProperty("font-size", fontSize);
 	}
 });
