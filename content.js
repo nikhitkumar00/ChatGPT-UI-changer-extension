52bf3acc2db350b657f66785253d4f76dae5290d
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-23 21:32:52 +0530

    added dark theme

---
+++ content.js
@@ -0,0 +1,16 @@
+function toggleDarkTheme() {
+	document.body.style.backgroundColor = "black";
+
+	document.documentElement.style.setProperty("--surface-secondary", "black");
+	document.documentElement.style.setProperty("--surface-primary", "black");
+	document.documentElement.style.setProperty("--surface-tertiary", "black");
+	document.documentElement.style.setProperty("font-size", "18px");
+}
+
+toggleDarkTheme();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.action === "toggleDarkTheme") {
+		toggleDarkTheme();
+	}
+});
